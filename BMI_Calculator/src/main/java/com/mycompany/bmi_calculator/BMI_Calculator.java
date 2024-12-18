1a725abfb53c1047087fa9d6994034570a0a5b16
Victor <147107766+Crosby01@users.noreply.github.com>
2024-12-18 11:15:31 +0100

    Add files via upload

---
+++ BMI_Calculator/src/main/java/com/mycompany/bmi_calculator/BMI_Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bmi_calculator;
+
+/**
+ *
+ * @author Victor
+ */
+public class BMI_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
